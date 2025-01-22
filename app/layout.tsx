e0ac50e2a68caab0da707f794c758fcfac4287fc
Thang <thang.edreams@gmail.com>
2025-01-22 17:53:18 +0100

    Add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -32,6 +33,7 @@ export default function RootLayout({
           {children}
         </div>
       </body>
+      <GoogleAnalytics gaId="G-XQE7DT6EXW" />
     </html>
   );
 }
